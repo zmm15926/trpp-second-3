2c106f9321f123bcc9c9452b3f21692f11dc3e67
zmm15926 <merdanova06@mail.ru>
2026-03-31 05:21:20 -0700

    Update HealthResponse.java

--- src/main/java/ru/mirea/trpp_second_3/controllers/HealthResponse.java
+++ src/main/java/ru/mirea/trpp_second_3/controllers/HealthResponse.java
@@ -1,1 +1,20 @@
+package ru.mirea.trpp_second_3.controllers;
 
+import io.micronaut.core.annotation.Introspected;
+
+@Introspected
+public class HealthResponse {
+    private String status;
+
+    public HealthResponse(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
